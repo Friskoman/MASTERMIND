963ce7aaf807e250832f0382ae3c0e7d6f62d068
Friskoman <72226320+Friskoman@users.noreply.github.com>
2022-02-17 11:39:48 +0100

    Add files via upload
    een functie die alle mogelijk combinaties die de computer kan doen in een lijst zet

---
+++ all_combinations.py
@@ -0,0 +1,10 @@
+max = 6
+allGuesses = []
+
+for s in range(max):
+    for e in range(max):
+        for x in range(max):
+            for y in range(max):
+                allGuesses.append([s,e,x,y])
+# print(allGuesses)
+
